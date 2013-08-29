230ebd8f646e724aad6a665012e0e4791acfef86
Patrick Drechsler <socialcoding@pdrechsler.de>
2013-08-29 20:23:21 +0200

    clean frontend using 'yo angular'

---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngdemoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
