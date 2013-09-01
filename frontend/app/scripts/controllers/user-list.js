408b2300b7b02567c262e9a2fd7914d496aafced
Patrick Drechsler <socialcoding@pdrechsler.de>
2013-09-01 10:05:10 +0200

    added user-list

---
+++ frontend/app/scripts/controllers/user-list.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngdemoApp')
+  .controller('UserListCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
