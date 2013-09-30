e259e447a6ca61510c0adbaea1449a6a3d148d42
Patrick Drechsler <socialcoding@pdrechsler.de>
2013-09-30 13:25:55 +0200

    removed tests

---
+++ frontend/test/spec/controllers/controllers.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Controller: ngdemoApp.controllers', function () {
+
+  /* karma test runner fails if it can't find a test... */
+  it('should always be true', function () {
+    expect(true).toBe(true);
+  });
+});
